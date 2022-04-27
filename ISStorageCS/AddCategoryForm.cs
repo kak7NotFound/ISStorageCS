4d9da385eaf02317f933bb6dd7e43a63b9df5b15
kak7 <38856979+kaka0monstr@users.noreply.github.com>
2022-04-27 07:48:29 +0300

    sdf

---
+++ ISStorageCS/AddCategoryForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ISStorageCS
+{
+    public partial class AddCategoryForm : Form
+    {
+        public AddCategoryForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Program.database.ExecuteNonQuery($"create table {textBox1.Text} (item text, weight int, count int, owner text)");
+            Close();
+        }
+    }
+}
--- ISStorageCS/WorkingSpace.Designer.cs
+++ ISStorageCS/WorkingSpace.Designer.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel;
 
-namespace ISStorageCStextеу
+namespace ISStorageCS
 {
     partial class WorkingSpace
     {
@@ -31,29 +31,139 @@ namespace ISStorageCStextеу
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(196, 23);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(488, 208);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 14);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Категория";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Предметов в категории";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 14);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Кол-во предметов";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(12, 34);
+            this.comboBox1.Location = new System.Drawing.Point(12, 40);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(172, 21);
-            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 252);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 50);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Добавить категорию";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(256, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 50);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Изменить категорию";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(484, 252);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(200, 50);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Удалить категорию";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(156, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 173);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(156, 20);
+            this.textBox2.TabIndex = 12;
             // 
             // WorkingSpace
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(382, 363);
+            this.ClientSize = new System.Drawing.Size(716, 318);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "WorkingSpace";
             this.Text = "Рабочее пространство";
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.DataGridView dataGridView1;
 
         #endregion
     }
